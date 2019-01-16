2e29c7a1056e47aab6c5d267c6a1030b5c212677
Kritsada Dechawantana <kritsada@perfenterprise.com>
2019-01-16 10:28:47 +0700

    Added test project

---
+++ q1/loadapi.Test/InterestCalculatorTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace loadapi.Test
+{
+    public class InterestCalculatorTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
